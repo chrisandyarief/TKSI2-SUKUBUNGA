7ac30937d6a599b090a8dbe47580662abf900664
Chrisandy Arief <chrisandyarief33@gmail.com>
2019-02-12 09:57:01 +0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<body>
+    <form method="post" action="bunga.php">
+        <input type="text" name="pinjaman" placeholder="Pinjaman">
+        <input type="text" name="bunga" placeholder="Bunga">
+        <input type="text" name="tenor" placeholder="Tenor">
+        <input type="text" name="admin" placeholder="Admin">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
